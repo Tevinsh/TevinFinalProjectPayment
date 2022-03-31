5bfb00fb31c9af4a1e167b81f8b9caea2159fc77
tevinshidqi <pangeran.found@gmail.com>
2022-03-31 20:49:31 +0700

    swagger

--- Startup.cs
+++ Startup.cs
@@ -98,12 +98,12 @@ namespace PaymentAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            // if (env.IsDevelopment())
+            // {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PaymentAPI v1"));
-            }
+            // }
 
             app.UseDefaultFiles();
             app.UseStaticFiles();
